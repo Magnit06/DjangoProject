4b7772741210bfc817848618a6f2b4ca306187bf
Magnit06 <kfilipppov@mail.ru>
2021-11-01 01:13:51 +0400

    Похоже на итог ) Генерация работает

--- samplesite/bboard/management/commands/populate_db.py
+++ samplesite/bboard/management/commands/populate_db.py
@@ -1,4 +1,4 @@
-from random import random
+from random import choice
 
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
@@ -20,40 +20,40 @@ class Command(BaseCommand):
 		u = [x for x in range(1, 1001)]  # 1000
 		name = "Рубрика"
 
-		print("Генерируем суперпользователя")
+		self.stdout.write(self.style.SUCCESS('Генерируем суперпользователя'))
 		User.objects.create_superuser(username="admin", email="kfilipppov@mail.ru", password="admin")
 
-		print("Генерируем первую рубрику 'Без рубрики'")
+		self.stdout.write(self.style.SUCCESS('Генерируем первую рубрику "Без рубрики"'))
 		Rubric.objects.create(name="Без рубрики")
 
-		print("Генерируем 9 оставшихся рубрик")
+		self.stdout.write(self.style.SUCCESS('Генерируем 9 оставшихся рубрик'))
 		for i in range(9):
 			Rubric.objects.create(name=name + str(i + 1))
-		print("Рубрики созданы")
+		self.stdout.write(self.style.SUCCESS('Рубрики созданы'))
 
-		print("Генерируем 1000 пользователей")
+		self.stdout.write(self.style.SUCCESS('Генерируем 1000 пользователей'))
 		for i in range(1000):
 			User.objects.create_user(username=user_name + str(i + 1),
 									 email=user_name + str(i + 1) + "@bboard.ru",
 									 password=user_name + str(i + 1))
-		print("1000 пользователей создано")
+		self.stdout.write(self.style.SUCCESS('1000 пользователей создано'))
 
-		print("Генерируем для каждого пользователя 1000 объявлений")
+		self.stdout.write(self.style.SUCCESS('Генерируем для каждого пользователя 1000 объявлений'))
 		users = User.objects.all()
 		for user in users:
 			for i in range(1000):
 				Bb.objects.create(title=title + " " + str(i + 1) + " " + str(user.username),
 								  content=content + " " + str(i + 1) + " " + str(user.username),
-								  price=float("".join([random.choice(num) for _ in range(6)])),
-								  rubric=Rubric.objects.get(pk=random.choice(r)),
+								  price=float("".join([choice(num) for _ in range(6)])),
+								  rubric=Rubric.objects.get(pk=choice(r)),
 								  author=User.objects.get(pk=user.pk))
-		print("1000 объявлений для каждого пользователя созданы")
+		self.stdout.write(self.style.SUCCESS('1000 объявлений для каждого пользователя созданы'))
 
-		print("Генерируем для каждого объявления 1000 комментариев")
+		self.stdout.write(self.style.SUCCESS('Генерируем для каждого объявления 1000 комментариев'))
 		bbs = Bb.objects.all()
 		for bb in bbs:
 			for i in range(1000):
-				author = User.objects.get(pk=random.choice(u))
+				author = User.objects.get(pk=choice(u))
 				Comment.objects.create(author=author,
 									   content=comment + " " + str(i + 1) + " " + str(author.username),
 									   bb=bb)
